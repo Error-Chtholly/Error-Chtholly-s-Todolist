f56fd12096fd951ef2d05b26aa1287919594d9a1
Error-Chtholly <1696304992@qq.com>
2024-07-19 20:34:37 +0800

    更新

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ addListBtn.onclick = function () {
 
     //给li元素添加删除按钮
     let btn = document.createElement("button"); //创建按钮元素
-    btn.innerText = "删除"; //设置按钮的文本内容
+    btn.innerText = "Delete"; //设置按钮的文本内容
     li.appendChild(btn); //将按钮添加到li元素中 appendChild
     let btn2 = document.createElement("button"); //创建按钮元素
-    btn2.innerText = "已完成"; //设置按钮的文本内容
+    btn2.innerText = "Already Done"; //设置按钮的文本内容
     li.appendChild(btn2); //将按钮添加到li元素中 appendChild
     btn.onclick = function () { //给按钮添加点击事件处理函数
         //删除li元素 remove() 方法可以删除元素，removeChild() 方法可以删除子元素
@@ -39,7 +39,7 @@ addListBtn.onclick = function () {
         li2.innerText = value; //设置li元素的文本内容为输入框中的值 innerText
         ul2.appendChild(li2); //将li元素添加到ul元素中 appendChild
         let btn3 = document.createElement("button"); //创建按钮元素
-        btn3.innerText = "彻底删除"; //设置按钮的文本内容
+        btn3.innerText = "Delete Completely"; //设置按钮的文本内容
         li2.appendChild(btn3); //将按钮添加到li元素中 appendChild
 
         btn3.onclick = function () { //给按钮添加点击事件处理函数
